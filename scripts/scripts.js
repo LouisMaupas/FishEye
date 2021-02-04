1292e1a7149a9024c678b402387aed8904db7e7d
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-02-04 19:15:35 +0100

    added modal windows

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+// Toute la gestion des événements (par exemple, les clics et les pressions au clavier)
+// doit être configurée (utilisez KeyboardEvent.key ou KeyboardEvent.code.).
+
+/*
+const portrait = document.getElementById('portrait')
+const articleOne = document.getElementById('article-one')
+const tags = ['portrait', 'art', 'fashion', 'architecture', 'travel', 'sport', 'animals', 'events']
+const thubOne = ['portrait', 'travel', 'animals', 'events']
+const thubTwo = ['sport', 'architecture']
+const thubThree = ['art', 'fashion', 'events']
+const thubFour = ['portrait', 'travel']
+const thubFive = ['fashion', 'sport', 'animals', 'events']
+const thubSix = ['architecture', 'events'],
+    art = document.getElementById('art'),
+    fashion = document.getElementById("fashion"),
+    architecture = document.getElementById("architecture"),
+    travel = document.getElementById("travel"),
+    sport = document.getElementById("sport"),
+    animals = document.getElementById("animals"),
+    events = document.getElementById("events"),
+    articleOne = document.getElementById("article-one"),
+    articleTwo = document.getElementById("article-two");
+
+portrait.addEventListener('click', function () {
+  articleOne.style.display = 'none'
+  portrait.style.backgroundColor = "red"
+})
+*/
+
+/*
+readJson () {
+  fetch('file:///C:/Users/Louis/Sync/Louis%20Perso/OCR%20+%20pole%20emploi/Alternance%20dev%20front/Projet%206/FishEye/datas.json')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('HTTP error' + Response.status);
+        }
+        return res.json();
+      })
+      .then(json => {
+        this.users = json;
+        console.log(this.users);
+      })
+      .catch(function() {
+        this.dataError = true;
+      })
+  }
+ */

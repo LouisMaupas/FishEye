03ec70636f6a8f6929a16ce847c33b1da10df996
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-02-05 18:34:54 +0100

    working on json fetch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,21 +27,97 @@ portrait.addEventListener('click', function () {
 })
 */
 
-/*
-readJson () {
-  fetch('file:///C:/Users/Louis/Sync/Louis%20Perso/OCR%20+%20pole%20emploi/Alternance%20dev%20front/Projet%206/FishEye/datas.json')
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('HTTP error' + Response.status);
-        }
-        return res.json();
-      })
-      .then(json => {
-        this.users = json;
-        console.log(this.users);
-      })
-      .catch(function() {
-        this.dataError = true;
-      })
+fetch('http://127.0.0.1:5500/datas.json')
+// 1er promesse on recupere la reponse
+  .then(res => {
+    // S'il y a un soucis ...
+    if (!res.ok) {
+      throw new Error('HTTP error' + Response.status)
+    }
+    // Si tout va bien on jsonifie la réponse
+    return res.json()
   }
- */
+  )
+  // 2nd promesse
+  .then(data => {
+    console.log(data.photographers[0])
+    const eulData = data.photographers[1]
+    console.log(eulData)
+    let maVariable = document.getElementById('divLibre');
+    maVariable.innerText = eulData
+  }
+  )
+  // si la promesse est rejetée
+  .catch(function () {
+    this.dataError = true
+  }
+  )
+
+// const photographerOne = {}
+// let maVariable = document.getElementById('divLibre');
+/*
+const photographerTwo
+let photographerThree
+let photographerFour
+let photographerFive
+let photographerSix
+*/
+/*
+fetch('http://127.0.0.1:5500/datas.json')
+  .then(response => response.json())
+  .then(data => console.log((data.photographers[0])))
+*/
+/*
+fetch('http://127.0.0.1:5500/datas.json')
+  .then(response => response.json())
+  .then(data => console.log(data.photographers[1]))
+*/
+
+/*
+function recup () {
+  fetch('http://127.0.0.1:5500/datas.json')
+    .then(response => response.json())
+    .then(data => {
+      maVariable.innerHTML = data.photographers[1]
+    })
+}
+recup()
+*/
+
+/*
+function creatPhotographe (name, id, city, country, tags, price, portrait, tagline) {
+  const objet = {}
+  objet.name = name
+  objet.id = id
+  objet.city = city
+  objet.country = country
+  objet.tags = tags
+  objet.price = price
+  objet.portrait = portrait
+  objet.tagline = tagline
+  return objet
+}
+*/
+
+/*
+let test = creatPhotographe(photographerOne)
+console.log('HEY YO' + test + 'HEY')
+*/
+/*
+let maVariable = document.getElementById('divLibre');
+const getData = async function () {
+  try {
+    const response = await fetch('http://127.0.0.1:5500/datas.json')
+    if (response.ok) {
+      const data = response.json()
+      console.log(data)
+      maVariable.innerHTML = data
+    } else {
+      console.error('Le serveur indiuque : ', response.status)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+getData()
+*/

6539a74ebf475546c71615984706c502d7e77c72
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-02-12 17:33:26 +0100

    test.js test de factory

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,9 @@ const ellie = new Photographer(930)
 photographers.push(ellie)
 
 // on distribue les images dans le dom à l'aide de (foreach) et renderHTML
+
+/* ALTERNATIVE */
+
+// DataHandler  : gère la récupération des données  que je récupère dans un objet {photographers: ..., media: ...}
+// Je passe cet objet à mon HomePageBuilder pour qu'il s'en serve pour construire le HTML et les events
+// HomePageBuilder : gère le rendu de la home page et ses événements

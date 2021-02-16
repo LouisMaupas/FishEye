a96dc0d4c88a6254ee46fa68d26920e9170c4b5c
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-02-16 14:36:25 +0100

    tidied js files

--- scripts/factory-photographer.js
+++ scripts/factory-photographer.js
@@ -1,12 +1,62 @@
-class Photographer {
-  constructor (name, id, city, country, tags, price, portrait, tagline) {
-    this.name = name
-    this.id = id
-    this.city = city
-    this.country = country
-    this.tags = tags
-    this.price = price
-    this.portrait = portrait
-    this.tagline = tagline
+// usine du modele des photographes
+class Photographers {
+  constructor (id) {
+    const url = 'http://127.0.0.1:5501/datas.json'
+    const response = fetch(url)
+    const MyPromiseOk = response.then(res => {
+      if (!res.ok) {
+        throw new Error('HTTP error' + Response.status)
+      }
+      return res.json()
+    })
+    const MyPromiseResult = MyPromiseOk.then(data => {
+      this.data = data.photographers[id]
+    })
   }
 }
+
+// créateur de photographes
+function runPhotographer (id) {
+  const factory = new Factory(id, 'photographers')
+  tableauPhotographes.push(factory.createObject(id, 'photographers'))
+  return tableauPhotographes
+}
+
+// les photographes
+const mimi = 0
+const ellie = 1
+const tracy = 2
+const nabeel = 3
+const rhode = 4
+const marcel = 5
+
+// création des photographes
+runPhotographer(mimi)
+runPhotographer(ellie)
+runPhotographer(tracy)
+runPhotographer(nabeel)
+runPhotographer(rhode)
+runPhotographer(marcel)
+
+console.log(tableauPhotographes)
+
+// pour assigner automatiquement
+// const nonAssingedphoto = {
+//   mimi: 0,
+//   ellie: 1,
+//   tracy: 2,
+//   nabeel: 3,
+//   rhode: 4,
+//   marcel: 5
+// }
+
+// let runAllPhotographers = function (a) {
+//   // for (let i; i <= a.length; i++) {
+//   //   a[i].push(tableauPhotographes)
+//   // }
+//   for (let key in a) {
+//     let value = a[key]
+//     console.log(key)
+//   }
+// }
+// runAllPhotographers(nonAssingedphoto)
--- scripts/factory.js
+++ scripts/factory.js
@@ -1,62 +1,23 @@
-// permet de créer l'objet photographe
-let datas = 'avant'
-
-const data = function (param) {
-  console.log(param)
-  debugger
-  return param
-}
-
-function getData () {
-  const url = 'http://127.0.0.1:5500/datas.json'
-  fetch(url).then(res => {
-    if (!res.ok) {
-      throw new Error('HTTP error' + Response.status)
-    } else {
-      res.json().then(data => {
-        debugger
-        console.log(data)
-        console.log(data.media)
-        data(data)
-        debugger
-      })
+// Usine generale
+class Factory {
+  constructor (id, type) {
+    this.createObject = function (id) {
+      let object
+      if (type === 'media') {
+        object = new Media(id)
+      } else if (type === 'photographers') {
+        object = new Photographers(id)
+      } else {
+        console.log('erreur lors de la création de l\'objet')
+      }
+      return object
     }
-  })
-}
-getData()
-
-data()
-
-/*
-function createPhotographer() {
-    return {
-      id: id,
-
-    }
-
-    constructor () {
-      const url = 'http://127.0.0.1:5500/datas.json'
-      const response = fetch(url)
-      const getData = response.then(res => {
-        if (!res.ok) { // HTTP-status is not 200-299
-          throw new Error('HTTP error' + Response.status)
-        }
-        return res.json()
-      })
-    }
-    //
-    getPhotographer () {
-      return photographer.find(i => i.id === id)
-    }
-
-    getMedia (id) {
-      let media
-      return media.find(i => i.id === id)
-    }
-
-    getPicturesByPhotographer (photographerId, mediaId) {
-      return pictures.filter(i => i.photographerId === photographerId)
-    }
-
   }
-  */
+}
+
+// stock de photographes et des photos
+const tableauPhotographes = []
+const tableauMedia = []
+
+// voir request ???
+// https://developer.mozilla.org/fr/docs/Web/API/Request
